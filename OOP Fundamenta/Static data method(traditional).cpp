821d784a1ba141c06e5c745bacab18e897473aee
Joy Nath <110628517+joynath12346@users.noreply.github.com>
2026-09-13 11:58:50 +0600

    Encapsulate Student class with getters and setters
    The Student class now has proper encapsulation with private attributes and public getter and setter methods. Additionally, a static method to get the student count has been implemented.

---
+++ OOP Fundamenta/Static data method(traditional).cpp
@@ -0,0 +1,106 @@
+/*
+
+Make the Student class properly encapsulated.
+
+Your name, id, dept, and cgpa must remain private.
+
+Add getter and setter functions for all four.
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Student {
+private:
+    string name = "Unknown";
+    int id = -1;
+    string dept = "Unknown";
+    float cgpa = -1;
+    static int  studentCount;
+    
+public:
+
+    
+
+
+    void setName(string name) {
+        if (name != "") this->name = name;
+    }
+    void setId(int id) {
+        if (id > 0)     this->id = id;
+    }
+    void setDept(string dept) {
+        this->dept = dept;
+    }
+    void setCgpa(float cgpa) {
+        if(0 <= cgpa && cgpa <= 4)  this->cgpa = cgpa;
+    }
+    
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return id;
+    }
+    string getDept() {
+        return dept;
+    }
+    float getCgpa() {
+        return cgpa;
+    }
+    
+    void updateStudent(string name , int id , string dept , float cgpa) {
+        setName(name);
+        setId(id);
+        setDept(dept);
+        setCgpa(cgpa);
+    }
+    
+
+    Student() {
+        studentCount++;
+    }
+   
+    Student(string name, int id){
+        setName(name);
+        setId(id);
+        studentCount++;
+    }
+    Student(string name, int id, string dept, float cgpa){
+        setName(name);
+        setId(id);
+        setDept(dept);
+        setCgpa(cgpa);
+        studentCount++;
+    }
+    
+     static int getStudentCount() {
+        return studentCount;
+    }
+    
+
+    void displayInfo() {
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Department: " << dept << endl;
+        cout << "CGPA: " << cgpa << endl;
+    }
+};
+
+int Student::studentCount = 0;
+
+int main() {
+
+    cout << Student::getStudentCount() << endl;
+
+    Student s1;
+    Student s2("Joy", 101);
+    Student s3("Rahim", 102, "CSE", 3.85);
+
+    cout << Student::getStudentCount() << endl;
+
+}
