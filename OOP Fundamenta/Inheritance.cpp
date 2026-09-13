d608a55a5871cec16341a615c943a0fbdca778c6
Joy Nath <110628517+joynath12346@users.noreply.github.com>
2026-09-13 15:27:49 +0600

    Create Inheritance.cpp

---
+++ OOP Fundamenta/Inheritance.cpp
@@ -0,0 +1,79 @@
+/*
+
+Add a function:
+
+removeStudent(int id)
+
+It should:
+
+Search for the student by ID.
+If found, remove that student from the vector.
+If not found, print "Student not found".
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Person {
+    string name;
+    int id;
+    
+public:
+    void setName(string name) {
+        this->name = name;
+    }
+    void setId(int id) {
+        this->id = id;
+    }
+    string getName() {
+        return name;
+    }
+    int getId() {
+        return id;
+    }
+    
+};
+
+
+class Student: public Person {
+    string dept = "Unknown";
+    float cgpa = -1;
+    
+public:
+
+    void setDept(string dept) {
+        this->dept = dept;
+    }
+    void setCgpa(float cgpa) {
+        if(0 <= cgpa && cgpa <= 4)  this->cgpa = cgpa;
+    }
+    
+    string getDept() {
+        return dept;
+    }
+    float getCgpa() {
+        return cgpa;
+    }
+    void displayInfo() {
+        cout << "Name: " << getName() << endl; // this the main magic here
+        cout << "ID: " << getId() << endl;
+        cout << "Department: " << getDept() << endl;
+        cout << "CGPA: " << getCgpa() << endl;
+    }
+};
+
+
+
+int main() {
+
+    Student s1;
+
+    s1.setName("Joy");
+    s1.setId(101);
+    s1.setDept("CSE");
+    s1.setCgpa(3.85);
+
+    s1.displayInfo();
+}
